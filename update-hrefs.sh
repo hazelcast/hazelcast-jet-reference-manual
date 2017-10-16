e893b44146868d849ad721c310f05d7c637d8586
Marko Topolnik <marko@hazelcast.com>
2017-10-16 15:01:49 +0200

    Make update-hrefs.sh update HREFs to Javadoc

--- update-hrefs.sh
+++ update-hrefs.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-if [[ $# -lt 2 ]] ; then
-    echo 'Updates the git branch name in hrefs in all *.md files.'
-    echo "Syntax: $0 <search_branch_name> <replace_with_branch_name>"
+if [[ $# -lt 1 ]] ; then
+    echo 'Updates the hrefs in all *.md files to point to a release version.'
+    echo "Syntax: $0 <jet_verson>"
     exit 0
 fi
 
-re="(/hazelcast-jet[^/]*/)(blob|tree)/$1/"
-perl_command="s!$re!/\1\2/$2/!"
+re="(/hazelcast-jet[^/]*/)(blob|tree)/master/"
+perl_command="s!$re!/\1\2/$1-maintenance/!"
 find docs -name \*.md | xargs egrep -l $re | xargs perl -pi -e $perl_command
+
+search_for="https://hazelcast-l337.ci.cloudbees.com/view/Jet/job/Jet-javadoc/javadoc/"
+replace_with="http://docs.hazelcast.org/docs/jet/$1/javadoc/"
+find docs -name \*.md | xargs egrep -l $search_for | xargs perl -pi -e "s!$search_for!$replace_with!"
