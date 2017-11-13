c87d0add97c0c08c8a9ab5eecc1656a83e5ba146
Marko Topolnik <marko@hazelcast.com>
2017-11-13 14:55:04 +0100

    Update update-hrefs.sh to public Javadoc link

--- update-hrefs.sh
+++ update-hrefs.sh
@@ -10,6 +10,6 @@ re="(/hazelcast-jet[^/]*/)(blob|tree)/master/"
 perl_command="s!$re!/\1\2/$1-maintenance/!"
 find docs -name \*.md | xargs egrep -l $re | xargs perl -pi -e $perl_command
 
-search_for="https://hazelcast-l337.ci.cloudbees.com/view/Jet/job/Jet-javadoc/javadoc/"
+search_for="http://docs.hazelcast.org/docs/jet/latest-dev/javadoc/"
 replace_with="http://docs.hazelcast.org/docs/jet/$1/javadoc/"
 find docs -name \*.md | xargs egrep -l $search_for | xargs perl -pi -e "s!$search_for!$replace_with!"
